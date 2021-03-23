f8c4bb33fa375ce954a548f0a9bbbc250c2442b2
Jinay01 <jinayparekh2001@gmail.com>
2021-03-23 10:49:31 +0530

    requirements.txt and test data added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tabula
 # pip3 install tabula-py
-df = ("E:/USEFUL/OCR/Geeks For Geeks/Result of 60004190056 for Semester I held in DECEMBER , 2019.PDF")
-output = ("E:/USEFUL/OCR/Geeks For Geeks/test1.csv")
+df = ("E:/USEFUL/OCR/Geeks For Geeks/pdf files/Result of 60004190056 for Semester II held in MAY , 2020.PDF")
+output = ("E:/USEFUL/OCR/Geeks For Geeks/csv files/test2.csv")
 tabula.convert_into(df, output, output_format="csv", pages="all", stream=True)
